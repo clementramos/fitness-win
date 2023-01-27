670f577f25c10cb8313f600d54bfb3087bf75c14
Clement Ramos <ramos.clement@outlook.fr>
2023-01-27 09:55:49 +0100

    deleted dark mode

--- pages/contact.js
+++ pages/contact.js
@@ -40,8 +40,8 @@ const Contact = () => {
                     onSubmit={submitForm}
                 >
                 <div>
-                    <label for="name" class="text-left block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Votre prénom</label>
-                    <input className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                    <label for="name" class="text-left block mb-2 text-sm font-medium text-gray-900 text-gray-300">Votre prénom</label>
+                    <input className="shadow-sm bg-gray-700 border border-gray-600 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-primary-500 focus:border-primary-500 shadow-sm-light"
                         required
                         placeholder="Prénom*"
                         type={"text"}
@@ -50,8 +50,8 @@ const Contact = () => {
                 </div>
                   
                 <div>
-                    <label for="lastname" class="text-left block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Votre nom</label>
-                    <input className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                    <label for="lastname" class="text-left block mb-2 text-sm font-medium text-gray-900 text-gray-300">Votre nom</label>
+                    <input className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-primary-500 focus:border-primary-500 shadow-sm-light"
                         required
                         placeholder="Nom*"
                         type={"text"}
@@ -60,23 +60,23 @@ const Contact = () => {
                 </div>
 
                 <div>
-                    <label for="mail" class="text-left block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Votre mail</label>
-                    <input className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                    <label for="mail" class="text-left block mb-2 text-sm font-medium text-gray-900 text-gray-300">Votre mail</label>
+                    <input className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-primary-500 focus:border-primary-500 shadow-sm-light"
                         required
                         placeholder="Mail*"
                         type={"mail"} />
                 </div>
 
                 <div>
-                    <label for="phone" class="text-left block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Votre téléphone</label>
-                    <input className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                    <label for="phone" class="text-left block mb-2 text-sm font-medium text-gray-900 text-gray-300">Votre téléphone</label>
+                    <input className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-primary-500 focus:border-primary-500 shadow-sm-light"
                         required
                         placeholder="Téléphone*"
                         type={"tel"}
                         minLenght={10}
                         maxLength={10} />
                 </div>
-                    <button type="submit" className="py-3 px-5 text-sm w-full font-medium text-center text-white rounded-lg bg-green-500  hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+                    <button type="submit" className="py-3 px-5 text-sm w-full font-medium text-center text-white rounded-lg bg-green-500  hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 bg-primary-600 hover:bg-primary-700 focus:ring-primary-800">
                         Envoyer
                     </button>
                 </form>

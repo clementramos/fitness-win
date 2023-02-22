147b8706ea8cef00f931f0d177db2ea152555138
Clement Ramos <ramos.clement@outlook.fr>
2023-02-22 20:21:20 +0100

    modified API route and stripe access w/ supabase

--- pages/api/create-stripe-customer.js
+++ pages/api/create-stripe-customer.js
@@ -2,6 +2,10 @@ import initStripe from "stripe";
 import { supabase } from "../../utils/supabase";
 
 const handler = async (req, res) => {
+    if (req.query.API_ROUTE_SECRET !== process.env.API_ROUTE_SECRET) {
+        return res.status(401).send("Vous n'êtes pas autorisé à appeler cette API.");
+    }
+    
     const stripe = initStripe(process.env.STRIPE_SECRET_KEY);
 
     const customer = await stripe.customers.create({
